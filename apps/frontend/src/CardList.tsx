05213d4a4314f1f70f323ee9a447bac62cbc6e29
Kody Fintak <kody.fintak@opensesame.com>
2025-05-02 09:55:23 -0400

    actually fetch courses

--- apps/frontend/src/CardList.tsx
+++ apps/frontend/src/CardList.tsx
@@ -1,8 +1,27 @@
+import {useEffect, useState} from 'react'
+
+interface Course {
+  name: string;
+  category: string;
+  description: string;
+  rating: number;
+}
+
 interface CardListProps {
-  fetchCourses: () => void;
+  fetchCourses: () => Course[];
 }
 
 const CardList = ({ fetchCourses }: CardListProps) => {
-  return <div data-testid="card-name">Name</div>;
+  const [courses, setCourses] = useState<Course[]>()
+
+  useEffect(() => {
+    const courses = fetchCourses()
+    setCourses(courses)
+  }, [])
+
+  return courses?.map(course => {
+    return <div data-testid="card-name">{course.name}</div>;
+  })
 };
+
 export default CardList;
