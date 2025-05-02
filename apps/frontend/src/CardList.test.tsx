98dcae416757a4fb36015e0027ed1cd26d015c5a
Kody Fintak <kody.fintak@opensesame.com>
2025-05-02 10:07:01 -0400

    add card description

--- apps/frontend/src/CardList.test.tsx
+++ apps/frontend/src/CardList.test.tsx
@@ -12,5 +12,7 @@ describe("CardList", () => {
     };
     render(<CardList fetchCourses={() => [course]} />);
     expect(screen.getByTestId("card-name")).toHaveTextContent("Name");
+    expect(screen.getByTestId("card-description")).toHaveTextContent("Description");
+    expect(screen.getAllByTestId('card')).toHaveLength(1)
   });
 });
